393a387c868e58b531ebb412df96a99f04f69d1f
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-06-21 08:05:22 +0500

    Update app.js

--- carousel/app.js
+++ carousel/app.js
@@ -1,10 +1,14 @@
-
+var counter = 0;
 function Next(){
     var img = document.getElementById('my-img')
     console.log('next')
+    count++
 }
 
 function Prev(){
     var img = document.getElementById('my-img')
     console.log('previous')
+    count--
 }
+
+
